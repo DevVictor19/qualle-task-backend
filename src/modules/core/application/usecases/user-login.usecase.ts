d7c7eaf151a76524dbb6530a9c0b2334f0ee43a4
DevVictor19 <antoniovictor12@live.com>
2026-06-23 20:00:54 -0300

    feat: add user login usecase

---
+++ src/modules/core/application/usecases/user-login.usecase.ts
@@ -0,0 +1,51 @@
+import { UseCase } from '@/shared/application';
+import { Injectable } from '@nestjs/common';
+import { UserLoginInputDto, UserLoginOutputDto } from '../dtos';
+import { UserRepository } from '../../domain';
+import { HashService, JwtService } from '../services';
+
+@Injectable()
+export class UserLoginUseCase implements UseCase<
+  UserLoginInputDto,
+  UserLoginOutputDto
+> {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService,
+    private readonly hashService: HashService,
+  ) {}
+
+  async execute(input: UserLoginInputDto): Promise<UserLoginOutputDto> {
+    const user = await this.userRepository.findByEmail(input.email);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const isValidPassword = await this.hashService.compare(
+      input.password,
+      user.password,
+    );
+    if (!isValidPassword) {
+      throw new Error('Invalid password');
+    }
+
+    const oneHourInSeconds = 60 * 60;
+    const sevenDaysInSeconds = 60 * 60 * 24 * 7;
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.sign(
+        { userId: user.id, type: 'access_token' },
+        oneHourInSeconds,
+      ),
+      this.jwtService.sign(
+        { userId: user.id, type: 'refresh_token' },
+        sevenDaysInSeconds,
+      ),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+}
