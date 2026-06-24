11d78577298f83e343bb05034a9e2aba6e9ed7cf
DevVictor19 <antoniovictor12@live.com>
2026-06-24 10:44:08 -0300

    feat: add validation to input

--- src/modules/core/presentation/graphql/inputs/signup.input.ts
+++ src/modules/core/presentation/graphql/inputs/signup.input.ts
@@ -1,13 +1,27 @@
 import { Field, InputType } from '@nestjs/graphql';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 @InputType()
 export class SignupInput {
   @Field()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
   name: string;
 
   @Field()
+  @IsEmail()
   email: string;
 
   @Field()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(72)
   password: string;
 }
