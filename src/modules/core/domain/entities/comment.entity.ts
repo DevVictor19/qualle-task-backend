d28cc8d96aab50966a8d8f0aba811625a44b235c
DevVictor19 <antoniovictor12@live.com>
2026-06-23 22:51:19 -0300

    feat: update comment relations

--- src/modules/core/domain/entities/comment.entity.ts
+++ src/modules/core/domain/entities/comment.entity.ts
@@ -1,26 +1,36 @@
 import { BaseEntity, BaseEntityProps } from '@/shared/domain';
+import { TaskEntity } from './task.entity';
+import { UserEntity } from './user.entity';
 
 export interface CommentEntityProps extends BaseEntityProps {
   content: string;
   userId: string;
   taskId: string;
+  task?: TaskEntity;
+  user?: UserEntity;
 }
 
 export class CommentEntity extends BaseEntity {
   content: string;
   userId: string;
   taskId: string;
+  task?: TaskEntity;
+  user?: UserEntity;
 
   private constructor({
     content,
     userId,
     taskId,
+    task,
+    user,
     ...base
   }: CommentEntityProps) {
     super(base);
     this.content = content;
     this.userId = userId;
     this.taskId = taskId;
+    this.task = task;
+    this.user = user;
   }
 
   static create(props: CommentEntityProps): CommentEntity {
